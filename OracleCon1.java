e5f9ed02a3fd43ce172169e088cbe059a6cf1607
RajdeepPodder <73603930+RajdeepPodder@users.noreply.github.com>
2021-05-07 21:14:53 +0530

    Update OracleCon1.java

--- OracleCon1.java
+++ OracleCon1.java
@@ -7,7 +7,7 @@ Class.forName("oracle.jdbc.driver.OracleDriver");
   
 //step2 create  the connection object  
 Connection con=DriverManager.getConnection(  
-"jdbc:oracle:thin:@localhost:1521:xe","system","kunalpodder1");  
+"jdbc:oracle:thin:@localhost:1521:xe","system"," ");  
   
 //step3 create the statement object  
 Statement stmt=con.createStatement();  
